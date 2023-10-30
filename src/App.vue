0aba68057bec1dad65737998fa5c091712dc3426
Jorabek1 <jorabeksuyunov69@gmail.com>
2023-10-30 12:45:29 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,11 @@
-<template lang="" v-if="count === 1" >
-  <div>
+<template>
+
     <h1 align='center'>Welcome to Vue js {{count}}</h1>
     
-  </div>
 </template>
-<template lang="" v-else>
 
-</template>
 <script setup>
-let count = 0
+
 </script>
 <style lang="">
   
